ae0750b6d95cf3ef9a96e5dd0f7b153078998bc9
kpkartikuzumaki111 <73732546+kpkartikuzumaki111@users.noreply.github.com>
2021-10-23 12:42:58 +0530

    Update questions.java

--- questions.java
+++ questions.java
@@ -1,1 +1,40 @@
 
+
+
+Recover a bst
+https://leetcode.com/problems/recover-binary-search-tree/
+class Solution {
+    
+    TreeNode a = null , b = null,prev = null;
+    public boolean recover(TreeNode root)
+    {
+        if(root == null) return false;
+        
+        if(recover(root.left)) return true;
+        
+        if(prev!=null && prev.val >  root.val)
+        {
+            b = root;
+            if(a == null) a = prev;
+            else return true;
+        }
+        
+        prev = root;
+        if(recover(root.right)) return true;
+        
+        return false;
+    }
+    
+    
+    public void recoverTree(TreeNode root) {
+      
+        recover(root);
+        
+        if(a!=null)
+        {
+            int temp = a.val;
+            a.val = b.val;
+            b.val = temp;
+        }
+    }
+}
