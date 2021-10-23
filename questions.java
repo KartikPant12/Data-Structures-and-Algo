c723fc8bfb2855392289711fd30cdbea61b625fe
kpkartikuzumaki111 <73732546+kpkartikuzumaki111@users.noreply.github.com>
2021-10-23 12:53:01 +0530

    Update questions.java

--- questions.java
+++ questions.java
@@ -38,3 +38,30 @@ class Solution {
         }
     }
 }
+
+
+
+
+// 
+to check if the tree is same or not!
+
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+     
+        
+        if(p == null && q == null) return true;
+        else if (p== null || q == null) return false;
+        
+        
+        if(p.val!=q.val) return false;
+        
+        
+    
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
+}
+
+
+
+
