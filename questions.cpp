9855a49b31c6e3582421f9ff43f3eec8e31fb37e
Divyam Sethi <52800799+DIVYUUMM@users.noreply.github.com>
2021-10-23 13:30:29 +0530

    Update questions.cpp

--- questions.cpp
+++ questions.cpp
@@ -87,3 +87,19 @@ int main()
  
     return 0;
 }
+
+/// path sum
+class Solution {
+public:
+    bool helper(TreeNode* root, int targetSum) {
+        if (!root) return false;
+        if (!root->left && !root->right) {
+            return targetSum - root->val == 0;
+        }
+        
+        return helper(root->left, targetSum - root->val) || helper(root->right, targetSum - root->val);
+    }
+    bool hasPathSum(TreeNode* root, int targetSum) {        
+        return helper(root, targetSum);
+    }
+};
