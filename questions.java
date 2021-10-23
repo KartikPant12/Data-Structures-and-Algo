12c1d7d0ec445f852d81a9b41e26da1042f7fbd8
kpkartikuzumaki111 <73732546+kpkartikuzumaki111@users.noreply.github.com>
2021-10-23 12:56:30 +0530

    Update questions.java

--- questions.java
+++ questions.java
@@ -63,5 +63,18 @@ class Solution {
 }
 
 
+// to check if the trees are symettric or not
+
+public boolean isSymmetric_(TreeNode root1,TreeNode root2){
+        
+        if(root1 == null && root2 == null) return true;
+        else if(root1 == null || root2 == null || root1.val!=root2.val) return false;
+        return isSymmetric_(root1.left,root2.right) && isSymmetric_(root1.right,root2.left);
+    }
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return isSymmetric_(root.left,root.right);
+    }
+
 
 
